1f7ad9a6564c9c2e8965c594779dce68ffca58a1
Valentin Molina <valentinmolina83@gmail.com>
2018-09-17 20:02:22 -0700

    Uploading hw files

---
+++ array1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main ()
+{
+	int i = 0, n, temp;
+	
+	printf ("Please enter the total amount of elemtents: ");
+	scanf ("%d", &i);
+	
+	//Creates array with number of elements that user inputted
+	int intergers [i], pos1 = 0, pos2 = i - 1;
+	n = i;
+	
+
+	printf ("Please enter the numbers: ");
+	
+	for (i = 0; i < n; i++)
+
+		scanf ("%d", &intergers [i]);
+
+	while ( pos1 != pos2 ) {
+
+		temp = intergers [pos1];
+		intergers [pos2] = intergers [pos1];
+		intergers [pos1] = temp;
+		pos1++;
+		pos2--;
+	}
+
+	printf ("The numbers in reverse order: ");
+
+	for (i = 0 ; i < n; i++)
+
+		printf ("%d ", intergers [i]);
+
+
+	printf ("\n");
+
+	printf ("Good bye!\n\n");
+
+
+	return 0;
+}
+ 
